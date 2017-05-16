d21edd1f4af67d57068bf67e12409c006c2b3ec8
DaveDLeague <dave.dunn@jointheleague.org>
2017-05-16 13:22:16 -0700

    Create BasicRightWallHugger.java

---
+++ src/demos/BasicRightWallHugger.java
@@ -0,0 +1,60 @@
+package Assignments;
+
+import java.io.IOException;
+
+import org.jointheleague.ecolban.rpirobot.IRobotAdapter;
+import org.jointheleague.ecolban.rpirobot.IRobotInterface;
+import org.jointheleague.ecolban.rpirobot.SimpleIRobot;
+
+public class BasicRightWallHugger extends IRobotAdapter {
+	Sonar sonar = new Sonar();
+	
+	public AssignmentCode(IRobotInterface iRobot) {
+		super(iRobot);
+	}
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("Try event listner, rev Monday 2030");
+		IRobotInterface base = new SimpleIRobot();
+		BasicRightWallHugger rob = new BasicRightWallHugger(base);
+		rob.setup();
+		while(rob.loop()){}
+		rob.shutDown();
+		
+	}
+
+	
+	
+	private void setup() throws Exception {
+		//SETUP CODE GOES HERE!!!!!
+	}
+	
+	private boolean loop() throws Exception{
+		//LOOP CODE GOES HERE!!!!!
+		readSensors(100);
+		
+		driveDirect(200, 200);
+		
+		if(isBumpRight()){
+			driveDirect(-200, 200);
+			sleep(100);
+		}
+		
+		return true;
+	}
+	
+	private void sleep(int amt){
+		try {
+			Thread.sleep(amt);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private void shutDown() throws IOException {
+		reset();
+		stop();
+		closeConnection();
+	}
+}
