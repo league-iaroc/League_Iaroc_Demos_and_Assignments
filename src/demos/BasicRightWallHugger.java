938551927f6075493fc0d4d1739c2aff2d19a535
DaveDLeague <dave.dunn@jointheleague.org>
2017-05-18 15:20:18 -0700

    Update BasicRightWallHugger.java

--- src/demos/BasicRightWallHugger.java
+++ src/demos/BasicRightWallHugger.java
@@ -1,4 +1,4 @@
-package Assignments;
+package demos;
 
 import java.io.IOException;
 
@@ -9,7 +9,7 @@ import org.jointheleague.ecolban.rpirobot.SimpleIRobot;
 public class BasicRightWallHugger extends IRobotAdapter {
 	Sonar sonar = new Sonar();
 	
-	public AssignmentCode(IRobotInterface iRobot) {
+	public BasicRightWallHugger(IRobotInterface iRobot) {
 		super(iRobot);
 	}
 
